fa5730edf4b7b593506330cd1c5926e4aaf0a3e2
Lucas Ângelo <dev.lucas.angelo@gmail.com>
2026-06-29 10:25:34 -0300

    Added SVG support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
 
     implementation(libs.coil.compose)
     implementation(libs.coil.gif)
+    implementation(libs.coil.svg)
     implementation(libs.coil.network.okhttp)
 
     implementation(libs.colorpicker.compose)
